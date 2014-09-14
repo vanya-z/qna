9313d706e442b653a2afba664a978e36bc14f4e3
Ivan Zakharov <vanya_z@list.ru>
2014-09-14 23:38:49 +1000

    add questions#show

--- spec/controllers/questions_controller_spec.rb
+++ spec/controllers/questions_controller_spec.rb
@@ -16,4 +16,16 @@ RSpec.describe QuestionsController, :type => :controller do
       expect(response).to render_template :index
     end
   end
+
+  describe 'GET #show' do
+    before { get :show, id: question }
+
+    it 'assigns the requested question to @question' do
+      expect(assigns(:question)).to eq question
+    end
+
+    it 'renders show view' do
+      expect(response).to render_template :show
+    end
+  end
 end
