fc46cfcc26c07fec86189015fbba4cebc35ba2b2
Ivan Zakharov <vanya_z@list.ru>
2014-09-17 23:54:29 +1000

    fix association

--- spec/factories/answers.rb
+++ spec/factories/answers.rb
@@ -3,11 +3,11 @@
 FactoryGirl.define do
   factory :answer do
     body "MyText"
-    association :question
+    question
   end
 
   factory :invalid_answer, class: "Answer" do
     body nil
-    association :question
+    question
   end
 end
