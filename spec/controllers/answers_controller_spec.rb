ba9b6323aabe996f618e03f47edddef0909ba412
Ivan Zakharov <vanya_z@list.ru>
2014-09-23 20:41:05 +1000

    create answer

--- spec/controllers/answers_controller_spec.rb
+++ spec/controllers/answers_controller_spec.rb
@@ -5,30 +5,45 @@ RSpec.describe AnswersController, :type => :controller do
   let(:answer) { create(:answer) }
 
   describe 'POST #create' do
-    context 'with valid attributes' do
-      it 'saves the new answer in the database' do
-        expect { post :create, question_id: question, answer: attributes_for(:answer) }.to change(Answer, :count).by(1)
+    context 'authenticated user' do
+
+      login_user
+
+      context 'with valid attributes' do
+        it 'saves the new answer in the database' do
+          expect { post :create, question_id: question, answer: attributes_for(:answer) }.to change(Answer, :count).by(1)
+        end
+
+        it 'redirects to question view' do
+          post :create, question_id: question, answer: attributes_for(:answer)
+          expect(response).to redirect_to question
+        end
       end
 
-      it 'redirects to question view' do
-        post :create, question_id: question, answer: attributes_for(:answer)
-        expect(response).to redirect_to question
+      context 'with invalid attributes' do
+        it 'does not save the answer' do
+          expect { post :create, question_id: question, answer: attributes_for(:invalid_answer) }.to_not change(Answer, :count)
+        end
+
+        it 're-renders question view' do
+          post :create, question_id: question, answer: attributes_for(:invalid_answer)
+          expect(response).to render_template 'questions/show'
+        end
       end
     end
 
-    context 'with invalid attributes' do
-      it 'does not save the answer' do
-        expect { post :create, question_id: question, answer: attributes_for(:invalid_answer) }.to_not change(Answer, :count)
-      end
-
-      it 're-renders question view' do
-        post :create, question_id: question, answer: attributes_for(:invalid_answer)
-        expect(response).to render_template 'questions/show'
+    context 'non-authenticated user' do
+      it 'redirects to new_user_session_path' do
+        post :create, question_id: question, answer: attributes_for(:answer)
+        expect(response).to redirect_to new_user_session_path
       end
     end
   end
 
   describe 'PATCH #update' do
+
+    login_user
+
     context 'valid attributes' do
       it 'assigns the requested answer to @answer' do
         patch :update, question_id: answer.question, id: answer, answer: attributes_for(:answer)
@@ -62,6 +77,7 @@ RSpec.describe AnswersController, :type => :controller do
   end
 
   describe 'DELETE #destroy' do
+    login_user
     before { answer }
 
     it 'deletes answer' do
