c99d31a484456e9d5774f21d62904e2ca019fe02
Ivan Zakharov <vanya_z@list.ru>
2014-09-29 19:46:24 +1000

    add user ref to question

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -19,7 +19,7 @@ class QuestionsController < ApplicationController
 
   def create
     @question = Question.new(question_params)
-
+    @question.user = current_user
     if @question.save
       redirect_to @question, flash: {notice: 'Your question successfully created.'}
     else
---
+++ spec/acceptance/edit_questions_spec.rb
@@ -0,0 +1,37 @@
+require_relative 'acceptance_helper'
+
+feature 'Edit question', %q{
+  In order to improve question
+  As an authenticated user
+  I want to be able to edit my question
+} do
+
+  given(:user) { create(:user) }
+  given(:question) { create(:question) }
+  given(:own_question) { create(:question, user: user) }
+
+  scenario 'Non-authenticated try to edit question' do
+    visit question_path(question)
+    expect(page).not_to have_link 'edit question'        
+  end
+
+  describe 'Authenticated user' do
+    scenario 'edits his question' do
+      sign_in(user)
+
+      visit question_path(own_question)
+      click_on 'edit question'
+      fill_in 'Title', with: 'New title'
+      fill_in 'Body', with: 'New body'
+      click_on 'Update Question'
+      expect(page).to have_content 'New title'
+      expect(page).to have_content 'New body'
+    end
+    
+    scenario 'try to edit other user\'s question' do
+      sign_in user
+      visit question_path(question)
+      expect(page).to_not have_link 'edit question'
+    end
+  end
+end
