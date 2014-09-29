eb4dadf299bf13551842fb4daea2fdb7ad1bb39e
Ivan Zakharov <vanya_z@list.ru>
2014-09-29 20:01:00 +1000

    destroy question by owner

--- spec/acceptance/destroy_question_spec.rb
+++ spec/acceptance/destroy_question_spec.rb
@@ -2,23 +2,31 @@ require_relative 'acceptance_helper'
 
 feature 'Destroy question', %q{
   As an authenticated user
-  I want to be able to destroy the question
+  I want to be able to destroy my question
 } do
 
   given(:user) { create(:user) }
   given(:question) { create(:question) }
+  given(:own_question) { create(:question, user: user) }
 
-  scenario 'Authenticated user destroys the question' do
-    sign_in(user)
+  describe 'Authenticated user' do
+    scenario 'deletes his question' do
+      sign_in(user)
+      visit question_path(own_question)
+      click_on 'delete question'
+      expect(current_path).to eq questions_path
+      expect(page).to have_content 'Question was successfully deleted'
+    end
 
-    visit question_path(question)
-    click_on 'delete question'
-    expect(current_path).to eq questions_path
-    expect(page).to have_content 'Question was successfully deleted'
+    scenario 'tries to delete other user\'s question' do
+      sign_in(user)
+      visit question_path(question)
+      expect(page).to_not have_link 'delete question'
+    end
   end
 
   scenario 'Non-authenticated try to edit question' do
     visit question_path(question)
-    expect(page).not_to have_link 'delete question'        
+    expect(page).to_not have_link 'delete question'
   end
 end
