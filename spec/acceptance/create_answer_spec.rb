004ef27e22f3d73d3fcdb42a61c326af5d69703b
Ivan Zakharov <vanya_z@list.ru>
2014-10-01 23:20:32 +1000

    user can't answer own question

--- spec/acceptance/create_answer_spec.rb
+++ spec/acceptance/create_answer_spec.rb
@@ -7,16 +7,25 @@ feature 'Create answer', %q{
 } do
 
   given(:user) { create(:user) }
+  given(:own_question) { create(:question, user: user) }
   given(:question) { create(:question) }
 
-  scenario 'Authenticated user creates an answer', js: true do
-    sign_in(user)
-    visit question_path(question)
-    fill_in 'Body', with: 'My answer body'
-    click_on 'Create'
+  describe 'Authenticated user' do
+    scenario 'answers other user\'s question', js: true do
+      sign_in(user)
+      visit question_path(question)
+      fill_in 'Body', with: 'My answer body'
+      click_on 'Create'
 
-    expect(page).to have_content 'My answer body'
-    expect(current_path).to eq question_path(question)
+      expect(page).to have_content 'My answer body'
+      expect(current_path).to eq question_path(question)
+    end
+
+    scenario 'tries to answer own question' do
+      sign_in(user)
+      visit question_path(own_question)
+      expect(page).not_to have_selector 'form#new_answer'
+    end
   end
 
   scenario 'Non-authenticated user tries to create answer' do    
