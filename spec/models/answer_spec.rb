79a6a5baa3733a70e200d832c7ec73f2c65dea1e
Ivan Zakharov <vanya_z@list.ru>
2014-10-02 21:55:41 +1000

    add accept

--- spec/models/answer_spec.rb
+++ spec/models/answer_spec.rb
@@ -1,7 +1,28 @@
 require 'rails_helper'
 
 RSpec.describe Answer, :type => :model do
+
   it { should belong_to :question }
   it { should belong_to :user }
   it { should validate_presence_of :body }
+
+  describe 'accept' do
+  	let(:question) { create(:question) }
+    let(:answer1) { create(:answer, question: question) }
+    let!(:answer2) { create(:answer, question: question, is_accepted: true) }
+
+    it 'sets the answer is_accepted true' do
+      answer1.accept
+      answer1.reload
+
+      expect(answer1.is_accepted).to eq true
+    end
+
+    it 'sets other question\'s answers is_accepted false ' do
+      answer1.accept
+      answer2.reload
+      
+      expect(answer2.is_accepted).to eq false
+    end
+  end
 end
