4c921c2127e6a90295297a8d67b2ebb06cdb51b0
Ivan Zakharov <vanya_z@list.ru>
2014-10-22 22:09:02 +1000

    comment test

---
+++ spec/acceptance/destroy_comment_spec.rb
@@ -0,0 +1,37 @@
+require_relative 'acceptance_helper'
+
+feature 'Destroy comment', %q{
+  In order to hide the comment
+  As an author of comment
+  I want to delete the comment
+} do
+
+  given!(:user) { create(:user) }
+  given!(:question) { create(:question) }
+  given!(:comment) { create(:comment, commentable: question, user: user) }
+  given(:another_user) { create(:user) }
+
+  describe 'Authenticated user' do
+    scenario 'delete own comment', js: true do
+      sign_in(user)
+      visit question_path(question)
+      click_on 'remove'
+
+      expect(page).to_not have_content comment.body
+      expect(current_path).to eq question_path(question)
+    end
+
+    scenario 'try to delete other user\'s comment' do
+      sign_in(another_user)
+      visit question_path(question)
+
+      expect(page).to_not have_link 'remove'
+    end
+  end
+
+  scenario 'Non-authenticated user tries to delete answer' do    
+    visit question_path(question)
+
+    expect(page).not_to have_link 'remove'
+  end
+end
--- spec/acceptance/edit_comment_spec.rb
+++ spec/acceptance/edit_comment_spec.rb
@@ -6,21 +6,15 @@ feature 'Edit comment', %q{
   I want to edit the comment
 } do
 
-  given(:user) { create(:user) }
-  given(:question) { create(:question) }
-  given(:comment) { create(:comment, commentable_id: question.id, commentable_type: question.class) }
-  given(:own_comment) { create(:comment, commentable_id: question.id, commentable_type: question.class, user: user) }
+  given!(:user) { create(:user) }
+  given!(:question) { create(:question) }
+  given!(:comment) { create(:comment, commentable: question, user: user) }
+  given(:another_user) { create(:user) }
 
   describe 'Authenticated user' do
     scenario 'edit own comment', js: true do
       sign_in(user)
       visit question_path(question)
-
-      within 'form#new_comment' do
-        fill_in 'Your comment', with: 'My comment body'
-        click_on 'Add comment'
-      end
-
       click_on 'edit'
       within 'form.edit_comment' do
         fill_in 'Edit comment', with: 'My edited comment body'
@@ -30,11 +24,18 @@ feature 'Edit comment', %q{
       expect(page).to have_content 'My edited comment body'
       expect(current_path).to eq question_path(question)
     end
+
+    scenario 'try to edit other user\'s comment' do
+      sign_in(another_user)
+      visit question_path(question)
+
+      expect(page).to_not have_link 'edit'
+    end
   end
 
-  scenario 'Non-authenticated user tries to create answer' do    
+  scenario 'Non-authenticated user tries to edit answer' do    
     visit question_path(question)
 
-    expect(page).not_to have_button 'Add comment'
+    expect(page).not_to have_link 'edit'
   end
 end
