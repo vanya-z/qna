6c0cdb2c637d5016aa9e5992a8fc9040747dbe0b
Ivan Zakharov <vanya_z@list.ru>
2014-10-22 23:01:15 +1000

    comments controller spec

--- spec/controllers/comments_controller_spec.rb
+++ spec/controllers/comments_controller_spec.rb
@@ -17,4 +17,51 @@ RSpec.describe CommentsController, :type => :controller do
       expect(assigns(:parent)).to eq answer
     end    
   end
+
+  describe 'PATCH #update' do
+    let(:comment) { create :comment }
+
+    context 'valid attributes' do
+      it 'assigns the requested comment to @comment' do
+        patch :update, id: comment, comment: attributes_for(:comment), format: :js
+        expect(assigns(:comment)).to eq comment
+      end
+
+      it 'changes comment attributes' do
+        patch :update, id: comment, comment: { body: 'edited body'}, format: :js
+        comment.reload
+        expect(comment.body).to eq 'edited body'
+      end
+
+      it 'renders update.js.erb' do
+        patch :update, id: comment, comment: attributes_for(:comment), format: :js
+        expect(response).to render_template :update
+      end
+    end
+
+    context 'invalid attributes' do
+      it 'it does not change comment\'s attributes' do
+        patch :update, id: comment, comment: {body: nil}, format: :js
+        expect(comment.body).to eq "My comment"
+      end
+
+      it 'renders update.js.erb' do
+        patch :update, id: comment, comment: {body: nil}, format: :js
+        expect(response).to render_template :update
+      end
+    end
+  end
+
+  describe 'DELETE #comment' do
+    let!(:comment) { create :comment }
+
+    it 'deletes comment' do
+      expect { delete :destroy, id: comment, format: :js }.to change(Comment, :count).by(-1)
+    end
+
+    it 'renders destroy.js.erb' do
+      delete :destroy, id: comment, format: :js
+      expect(response).to render_template :destroy
+    end
+  end
 end
