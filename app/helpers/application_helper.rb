b3c89bc38fcd100edcd477739caaa1d2ddc96141
vanya_z <vanya_z@list.ru>
2015-03-12 18:01:19 +0900

    add created_at helper

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,6 +1,10 @@
 module ApplicationHelper
 
-  def link_to_author(a)
-    a.user ? link_to(a.user.email, a.user) : "user deleted"
+  def link_to_author(object)
+    object.user ? link_to(object.user.email, object.user) : "user deleted"
+  end
+
+  def created_at(object)
+    object.created_at.year == Date.today.year ? object.created_at.to_formatted_s(:like_so) : object.created_at.to_formatted_s(:like_so_year)
   end
 end
